2926a008add69e7ec9fe33ee7be8e8967378d713
Taylor Leong <tjl2125@alum.barnard.edu>
2021-03-05 17:39:19 -0500

    editting html and css, functions need further editting

--- script.js
+++ script.js
@@ -1,15 +1,43 @@
-var currentHr = document.getElementsByClassName("input-group input-group-lg"); 
+var currentHr = document.getElementsByClassName("time-block"); 
 
+var currentDay = moment().format("dddd, MMMM Do YYYY"); 
+$("#currentDay").text(currentDay);
 
-var currentTime = moment().format("dddd, MMMM Do YYYY"); 
-$("#currentDay").text(currentTime);
+var timeNow = moment().format("HH:mm:ss")
 
 function startScheduler () {
-
 }
 
-function time {
-    if (currentHr == currentTime) {
-        document.style.background-color = $(".present"); 
+var checkTime = function () {
+
+    //Get Current time
+    var currentTime = moment().format('H');
+
+    //get all elements with class "taskarea"
+    var timeBlockElements = $(".textarea");
+
+    //loop through taskarea classes
+    for (var i = 0 ; i < timeBlockElements.length ; i++) {
+
+        //Get element i's ID as a string
+        var elementID = timeBlockElements[i].id;
+
+        //get element by ID
+        var manipID = document.getElementById(timeBlockElements[i].id)
+
+        //remove any old classes from element
+        //$(timeBlockElements[i].id).removeClass(".present .past .future");
+
+        // apply new class if task is present/past/future
+        if (elementID < currentTime) {
+            $(manipID).addClass(".past");
+        } else if (elementID > currentTime) {
+            $(manipID).addClass(".future");
+        } else {
+            $(manipID).addClass(".present");
+        }
     }
 }
+
+// checkTime every 5 minutes
+setInterval(checkTime(), (1000 * 60) * 5);
