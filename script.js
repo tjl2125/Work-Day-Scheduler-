16f5aafdcc09591bf0209b30e61da75d2ff62ccc
Taylor Leong <tjl2125@alum.barnard.edu>
2021-03-05 21:15:29 -0500

    added more functions

--- script.js
+++ script.js
@@ -1,44 +1,66 @@
 //var currentHr = document.getElementsByClassName("time-block"); 
+
+// update the current day at the top
+// var todayDate = moment().format("dddd, MMMM Do YYYY");
+// $("#currentDay").text(todayDate);
+// //get the current hour
+// var currentHour = moment().format("HH");
+// console.log(currentHour);
+
 var saveBtn = $('saveBtn'); 
 
 var currentDay = moment().format("dddd, MMMM Do YYYY"); 
 $("#currentDay").text(currentDay);
 
-var timeNow = moment().format("HH:mm:ss")
+var currentHour = moment().format("HH");
 
 function colorTime () {
-}
-
-var checkTime = function () {
-
-    //Get Current time
-    var currentTime = moment().format('H');
-
-    //get all elements with class "taskarea"
-    var timeBlockElements = $(".textarea");
-
-    //loop through taskarea classes
-    for (var i = 0 ; i < timeBlockElements.length ; i++) {
-
-        //Get element i's ID as a string
-        var elementID = timeBlockElements[i].id;
-
-        //get element by ID
-        var manipID = document.getElementById(timeBlockElements[i].id)
-
-        //remove any old classes from element
-        //$(timeBlockElements[i].id).removeClass(".present .past .future");
-
-        // apply new class if task is present/past/future
-        if (elementID < currentTime) {
-            $(manipID).addClass(".past");
-        } else if (elementID > currentTime) {
-            $(manipID).addClass(".future");
-        } else {
-            $(manipID).addClass(".present");
+    $('.time-block').each(function(index,item){
+        var currentInt = parseInt(currentHour);
+        var blockValue = parseInt($(item).data('hour'));
+        if (blockValue === currentInt){
+            $(item).addClass('present');
+        }else if (blockValue < currentInt) {
+            $(item).addClass('past');
+        }else {
+            $(item).addClass('future');
         }
-    }
-}
+    });
+};
 
-// checkTime every 5 minutes
-setInterval(checkTime(), (1000 * 60) * 5);
+colorTime(); 
+
+
+// var checkTime = function () {
+
+//     //Get Current time
+//     var currentTime = moment().format('H');
+
+//     //get all elements with class "taskarea"
+//     var timeBlockElements = $(".textarea");
+
+//     //loop through taskarea classes
+//     for (var i = 0 ; i < timeBlockElements.length ; i++) {
+
+//         //Get element i's ID as a string
+//         var elementID = timeBlockElements[i].id;
+
+//         //get element by ID
+//         var manipID = document.getElementById(timeBlockElements[i].id)
+
+//         //remove any old classes from element
+//         //$(timeBlockElements[i].id).removeClass(".present .past .future");
+
+//         // apply new class if task is present/past/future
+//         if (elementID < currentTime) {
+//             $(manipID).addClass(".past");
+//         } else if (elementID > currentTime) {
+//             $(manipID).addClass(".future");
+//         } else {
+//             $(manipID).addClass(".present");
+//         }
+//     }
+// }
+
+// // checkTime every 5 minutes
+// setInterval(checkTime(), (1000 * 60) * 5);
