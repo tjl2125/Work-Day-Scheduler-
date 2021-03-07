1280535b1fba8b8e1b7fa2d674b74a4556bff363
Taylor Leong <tjl2125@alum.barnard.edu>
2021-03-06 22:28:50 -0500

    fixed and broke and fixed functions

--- script.js
+++ script.js
@@ -1,47 +1,45 @@
 var currentDay = moment().format("dddd, MMMM Do YYYY"); 
 $("#currentDay").text(currentDay);
 
-var currentHour = moment().format("HH");
-
 //retrieving the text for tasks in time-blocks from localStorage
 $(document).ready(function(){
-  $('.textarea').each(function(){    
-      var id = $(this).attr('id');
+  $(".textarea").each(function(){
+      var id = $(this).attr("id");
       var value = localStorage.getItem(id);
       $(this).val(value);
-  }); 
+  });
 });
 
-//saving text for tasks to localStorage
-$('.savebtn').on('click', function(){
-  $('.textarea').each(function(){    
-      var id = $(this).attr('id');
-      var value = $(this).val();
-      localStorage.setItem(id, value);
-  });   
+$(".saveBtn").on("click", handleSave)
+
+//run the save function once button clicked
+function handleSave(event){
+  var value = $(this).siblings(".textarea").val();
+  var key = $(this).siblings(".textarea").attr("id");
+  localStorage.setItem(key, value);
+}
+
+//clears the local storage and reset value
+$(".clearBtn").on("click", function(){
+  $(".textarea").val("");
+  localStorage.clear();
 });
 
-$('.clearBtn').on('click', function(){
-  $('.textarea').each(function(){
-      $('.textarea').val('');    
-      var id = $(this).attr('id');
-      var value = $(this).val();
-      localStorage.setItem(id, value);
-  });   
-});
+
+var currentHour = moment().format("HH"); 
 
 //changes color of timeblocks depending on time 
 function colorTime() {
 
-    $(".textarea").each(function(index,item){ 
+    $(".time-block").each(function(index,item){
       var hourInt = parseInt(currentHour); 
-      var blockTime = parseInt($(item).data('index'));
+      var blockTime = parseInt($(item).attr("data-hour"));
       if(blockTime === hourInt){
-          $("textarea").addClass('present');
+        $(this).children("textarea").addClass("present");
       } else if (blockTime < hourInt) {
-        $("textarea").addClass('past');
+        $(this).children("textarea").addClass("past");
       } else {
-        $("textarea").addClass('future');
+        $(this).children("textarea").addClass("future");
       }
     });
 };
